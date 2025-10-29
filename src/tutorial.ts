5db3a3c4832761d6605e3c08a6f5c07433f22d95
Mupenzi Herve <mupenziherve04@gmail.com>
2025-10-29 23:13:07 +0200

    update

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -3,4 +3,10 @@ console.log('typescript tutorial')
 let awesomeName: string = 'shakeAndbake'
 awesomeName = 'something'
 awesomeName = awesomeName.toUpperCase()
-console.log(awesomeName)
+console.log(awesomeName);
+
+let amount: number = 20;
+amount = 21 -1;
+console.log(amount)
+
+lorem lorzzzzzzzzzzzzzzzzzzzzz
