d485a44acaa7181f0e69557a740c49a1e6f60daf
Mupenzi Herve <mupenziherve04@gmail.com>
2025-10-30 22:03:59 +0200

    update

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -7,6 +7,163 @@ console.log(awesomeName);
 
 let amount: number = 20;
 amount = 21 -1;
-console.log(amount)
+consol
 
-lorem lorzzzzzzzzzzzzzzzzzzzzz
+console.log('typescript tutorial')
+
+let awesomeName: string = 'shakeAndbake'
+awesomeName = 'something'
+awesomeName = awesomeName.toUpperCase()
+console.log(awesomeName)
+
+let amount: number = 20
+amount = 21 - 1
+consol
+
+
+console.log('typescript tutorial')
+
+let awesomeName: string = 'shakeAndbake'
+awesomeName = 'something'
+awesomeName = awesomeName.toUpperCase()
+console.log(awesomeName)
+
+let amount: number = 20
+amount = 21 - 1
+consol
+
+
+console.log('typescript tutorial')
+
+let awesomeName: string = 'shakeAndbake'
+awesomeName = 'something'
+awesomeName = awesomeName.toUpperCase()
+console.log(awesomeName)
+
+let amount: number = 20
+amount = 21 - 1
+consol
+
+
+console.log('typescript tutorial')
+
+let awesomeName: string = 'shakeAndbake'
+awesomeName = 'something'
+awesomeName = awesomeName.toUpperCase()
+console.log(awesomeName)
+
+let amount: number = 20
+amount = 21 - 1
+consol
+
+console.log('typescript tutorial')
+
+let awesomeName: string = 'shakeAndbake'
+awesomeName = 'something'
+awesomeName = awesomeName.toUpperCase()
+console.log(awesomeName)
+
+let amount: number = 20
+amount = 21 - 1
+consol
+
+console.log('typescript tutorial')
+
+let awesomeName: string = 'shakeAndbake'
+awesomeName = 'something'
+awesomeName = awesomeName.toUpperCase()
+console.log(awesomeName)
+
+let amount: number = 20
+amount = 21 - 1
+consol
+
+console.log('typescript tutorial')
+
+let awesomeName: string = 'shakeAndbake'
+awesomeName = 'something'
+awesomeName = awesomeName.toUpperCase()
+console.log(awesomeName)
+
+let amount: number = 20
+amount = 21 - 1
+consol
+
+console.log('typescript tutorial')
+
+let awesomeName: string = 'shakeAndbake'
+awesomeName = 'something'
+awesomeName = awesomeName.toUpperCase()
+console.log(awesomeName)
+
+let amount: number = 20
+amount = 21 - 1
+consol
+
+console.log('typescript tutorial')
+
+let awesomeName: string = 'shakeAndbake'
+awesomeName = 'something'
+awesomeName = awesomeName.toUpperCase()
+console.log(awesomeName)
+
+let amount: number = 20
+amount = 21 - 1
+consol
+
+console.log('typescript tutorial')
+
+let awesomeName: string = 'shakeAndbake'
+awesomeName = 'something'
+awesomeName = awesomeName.toUpperCase()
+console.log(awesomeName)
+
+let amount: number = 20
+amount = 21 - 1
+consol
+
+console.log('typescript tutorial')
+
+let awesomeName: string = 'shakeAndbake'
+awesomeName = 'something'
+awesomeName = awesomeName.toUpperCase()
+console.log(awesomeName)
+
+let amount: number = 20
+amount = 21 - 1
+consol
+
+console.log('typescript tutorial')
+
+let awesomeName: string = 'shakeAndbake'
+awesomeName = 'something'
+awesomeName = awesomeName.toUpperCase()
+console.log(awesomeName)
+
+let amount: number = 20
+amount = 21 - 1
+consol
+
+console.log('typescript tutorial')
+
+let awesomeName: string = 'shakeAndbake'
+awesomeName = 'something'
+awesomeName = awesomeName.toUpperCase()
+console.log(awesomeName)
+
+let amount: number = 20
+amount = 21 - 1
+consol
+
+console.log('typescript tutorial')
+
+let awesomeName: string = 'shakeAndbake'
+awesomeName = 'something'
+awesomeName = awesomeName.toUpperCase()
+console.log(awesomeName)
+
+let amount: number = 20
+amount = 21 - 1
+consol
+
+
