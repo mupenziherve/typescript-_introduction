0870227795c787a17820be7abc7ff1b97391854c
Mupenzi Herve <mupenziherve04@gmail.com>
2025-10-30 22:06:12 +0200

    update

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -122,15 +122,4 @@ let amount: number = 20
 amount = 21 - 1
 consol
 
-console.log('typescript tutorial')
-
-let awesomeName: string = 'shakeAndbake'
-awesomeName = 'something'
-awesomeName = awesomeName.toUpperCase()
-console.log(awesomeName)
-
-let amount: number = 20
-amount = 21 - 1
-consol
-
 
